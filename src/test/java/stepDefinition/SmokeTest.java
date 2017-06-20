3838dd882e36ca5d2b0eae870c133226d2878ddd
AbhishekSinghPaliwal <abhi.myth21@gmail.com>
2017-06-20 21:02:53 +0530

    //9th commit  for SmokeTest file

--- src/test/java/stepDefinition/SmokeTest.java
+++ src/test/java/stepDefinition/SmokeTest.java
@@ -18,6 +18,7 @@ public class SmokeTest
 	//6th commit by eclipse for SmokeTest file
 	//7th commit by eclipse for SmokeTest file
 	//8th commit by eclipse for SmokeTest file
+	//9th commit by eclipse for SmokeTest file
 	WebDriver driver;
 	@Given("^Open firefox and start application$")
 	public void open_firefox_and_start_application() throws Throwable {
